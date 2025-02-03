ca6194da8d554c1172034051b6beff18eed47ba3
amah <145132416+mahangel@users.noreply.github.com>
2025-02-03 15:43:15 -0500

    problem with "responsive"

--- src/pages/Guides.tsx
+++ src/pages/Guides.tsx
@@ -9,7 +9,6 @@ import {
   Image,
   List,
   Menu,
-  Responsive,
   Segment,
   Statistic,
   Sidebar,
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -8,7 +8,6 @@ import {
   Image,
   List,
   Menu,
-  Responsive,
   Segment,
   Statistic,
   Sidebar,
